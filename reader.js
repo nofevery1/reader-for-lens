b8ae1c3ba00e9da03a30b2bfe5590334fb62897a
Gareth Highnam <gareth862@gmail.com>
2015-04-09 11:34:57 -0500

    adding expand functionality

--- reader.js
+++ reader.js
@@ -1,3 +1,10 @@
 $(document).ready( function () {
+
   highlighter.highlightInit();
-})
+
+  //Prevent any action when reference resources are clicked
+  $('a.resource-reference').click( function(e) {
+    e.preventDefault();
+  });
+  expands.listener();
+});
